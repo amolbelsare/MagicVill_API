8b47ebfbddd9679f50de4fc332f19d52cd79aec5
Amol Belsare <amolsbelsare94@gmail.com>
2024-05-07 16:32:57 +0530

    Using Code First Approach generated table

---
+++ MagicVill_VillAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MagicVill_VillAPI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {               
+        }
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- MagicVill_VillAPI/Program.cs
+++ MagicVill_VillAPI/Program.cs
@@ -1,4 +1,6 @@
 using MagicVill_VillAPI.Logging;
+using MagicVill_VillAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
     .WriteTo.File("log/villalog.txt", rollingInterval:RollingInterval.Day).CreateLogger();
+
+builder.Services.AddDbContext<ApplicationDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 builder.Services.AddControllers(option =>
 {
